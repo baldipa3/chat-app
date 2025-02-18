ae936bbc56c56e00501f2152e3287f89d751ebd0
Pablo Baldini <pablo.baldini@ag-force.com>
2025-02-18 20:03:12 -0300

    run missing migration and update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_18_225417) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_18_225531) do
   create_table "messages", force: :cascade do |t|
     t.text "content"
     t.integer "user_id", null: false
@@ -44,6 +44,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_18_225417) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "surname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
